ed00fa95ff7fb7f2673ec650d44a5dc1a55816ea
Sarthak Mahajan <79055502+Sarthak4561@users.noreply.github.com>
2023-05-04 20:04:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const products = [
     id: 1,
     title: "Gaming Mouse",
     price: 14500,
-    description:`G502 X PLUS is the latest addition to legendary G502 lineage. 
+    description: `G502 X PLUS is the latest addition to legendary G502 lineage. 
     Reinvented with our first-ever LIGHTFORCE hybrid switches, 
     LIGHTSPEED pro-grade wireless, LIGHTSYNC RGB, HERO 25K sensor, and more.`,
     colors: [
@@ -24,7 +24,7 @@ const products = [
     id: 2,
     title: "Gaming Keyboard",
     price: 5990,
-    description:`Speed. Accuracy. Durability. Logitech G gaming keyboards are designed with the technology and materials required for high performance gaming.`,
+    description: `Speed. Accuracy. Durability. Logitech G gaming keyboards are designed with the technology and materials required for high performance gaming.`,
     colors: [
       {
         code: "black",
@@ -40,7 +40,7 @@ const products = [
     id: 3,
     title: "Boat Headphones",
     price: 2000,
-    description:`Speed. Accuracy. Durability. Logitech G gaming keyboards are designed with the technology and materials required for high performance gaming.`,
+    description: `Speed. Accuracy. Durability. Logitech G gaming keyboards are designed with the technology and materials required for high performance gaming.`,
     colors: [
       {
         code: "black",
@@ -56,7 +56,7 @@ const products = [
     id: 4,
     title: "Green Soul Gaming Chair",
     price: 35533,
-    description:`Speed. Accuracy. Durability. Logitech G gaming keyboards are designed with the technology and materials required for high performance gaming.`,
+    description: `Speed. Accuracy. Durability. Logitech G gaming keyboards are designed with the technology and materials required for high performance gaming.`,
     colors: [
       {
         code: "red",
@@ -72,7 +72,7 @@ const products = [
     id: 5,
     title: "XBox Controller",
     price: 6590,
-    description:`Speed. Accuracy. Durability. Logitech G gaming keyboards are designed with the technology and materials required for high performance gaming.`,
+    description: `Speed. Accuracy. Durability. Logitech G gaming keyboards are designed with the technology and materials required for high performance gaming.`,
     colors: [
       {
         code: "black",
@@ -149,4 +149,59 @@ close.addEventListener("click", () => {
   payment.style.display = "none";
 });
 
-console.log('yes');
+
+
+////////////////////////////////////////////////////////////////////////////
+
+
+// instantiate new modal
+const modal = new tingle.modal({
+  footer: true,
+  stickyFooter: false,
+  closeMethods: ['overlay', 'button', 'escape'],
+  closeLabel: "Close",
+  cssClass: ['custom-class-1', 'custom-class-2'],
+  onOpen: function () {
+    console.log('modal open');
+  },
+  onClose: function () {
+    console.log('modal closed');
+  },
+  beforeClose: function () {
+    // here's goes some logic
+    // e.g. save content before closing the modal
+    return true; // close the modal
+    // return false; // nothing happens
+  }
+});
+
+const formHTML = `
+  <form id="login-form">
+      <h1 class="payTitle spacess">Welcome to GExpress</h1>
+      <input type="email" id="email" style="display: block;" placeholder="email" class="payInput"required/>
+      <input placeholder="password" id="password" type="password" style="display: block;" class="payInput" required/>
+      <div onclick="sendPasswordReset()" class = "lessfont">
+        Forgot Password?
+      </div>
+      <button type="submit" class="payButton payButton-l">Login</button>
+      <button id="register-button" class="payButton payButton-r">Register</button>
+  </form>
+`
+
+modal.setContent(formHTML);
+
+const authHandler = () => {
+  document.getElementById("login-form").onsubmit = (e) => {
+    e.preventDefault()
+    console.log("submit form")
+    toggleSignIn();
+    modal.close();
+  }
+  document.getElementById("register-button").onclick = (e) => {
+    e.preventDefault()
+    console.log("register button")
+    handleSignUp();
+    modal.close();
+  }
+  modal.open();
+}
